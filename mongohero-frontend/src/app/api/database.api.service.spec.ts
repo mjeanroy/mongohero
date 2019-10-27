2ea48467fa7860482ea77b7c7947c55b28e793a0
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2019-10-27 23:06:51 +0100

    tests: add some unit test

---
+++ mongohero-frontend/src/app/api/database.api.service.spec.ts
@@ -0,0 +1,76 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Mickael Jeanroy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { DatabaseApiService } from './database.api.service';
+import { ApiModule } from './api.module';
+import { DatabaseModel } from '../models/database.model';
+
+describe('DatabaseApiService', () => {
+
+  let databaseApiService: DatabaseApiService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        ApiModule,
+      ],
+    });
+
+    // Inject the http service and test controller for each test
+    databaseApiService = TestBed.get(DatabaseApiService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should get database info', fakeAsync(() => {
+    const db = 'test';
+    const onSuccess = jasmine.createSpy('onSuccess');
+    const onError = jasmine.createSpy('onError');
+    const rsp: DatabaseModel = {
+      name: db,
+      sizeOnDisk: 4096,
+      empty: false,
+    };
+
+    databaseApiService.get(db).then(onSuccess).catch(onError);
+
+    const rq = httpTestingController.expectOne(`/api/databases/${db}`);
+    expect(rq.request.method).toBe('GET');
+    expect(rq.request.params.keys()).toBeEmpty();
+
+    rq.flush(null);
+
+    tick();
+
+    expect(onError).not.toHaveBeenCalled();
+    expect(onSuccess).toHaveBeenCalled();
+  }));
+});
